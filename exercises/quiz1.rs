c02fc0ca5e1574e0862635634b5e16e1da1badc3
Swadha <swadhavijay25@gmail.com>
2023-09-17 21:25:16 +0530

    If and Quiz exercise.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,15 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+pub fn calculate_price_of_apples(quantity : i32) -> i32{
+
+    let value = if quantity <= 40 {
+        quantity*2
+    } else{
+        quantity
+    };
+    value
+}
 
 // Don't modify this function!
 #[test]
